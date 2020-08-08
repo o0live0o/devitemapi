2094fab76400636f4fe6515b09f893714c0dfaaf
live0x <759163388@qq.com>
2020-08-08 12:34:31 +0800

    20200808

--- devitemapi/Infrastructure/Services/Interface/IOauthService.cs
+++ devitemapi/Infrastructure/Services/Interface/IOauthService.cs
@@ -7,7 +7,12 @@ namespace devitemapi.Infrastructure.Services.Interface
 {
 
     /*
-     * 
+     * 1.获取用户-角色关系
+     * 2.保存用户-角色关系
+     * 3.获取角色-菜单-操作关系
+     * 4.保存角色-菜单-操作关系
+     * 5.获取菜单-操作关系
+     * 6.保存菜单-操作关系
      */
     public interface IOauthService
     {
