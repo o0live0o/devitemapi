c0cc6d37db2f44e5a2643c8b0bb4eec76e08d047
o0live0o <live0x@outlook.com>
2021-01-20 08:43:53 +0800

    feat(snowflake):modify snowflake

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -21,16 +21,16 @@ namespace UnitTestProject1
             patern = @"^[^\s].*[^\s].*$";
             Regex regex = new Regex(patern);
 
-           Assert.AreEqual(false,regex.IsMatch(""));
-           Assert.AreEqual(false,regex.IsMatch(" "));
-           Assert.AreEqual(false,regex.IsMatch(" 123"));
-           Assert.AreEqual(true,regex.IsMatch("aaaaa"));
-           Assert.AreEqual(true, regex.IsMatch("1abic121"));
-           Assert.AreEqual(true, regex.IsMatch("A2isax"));            
-           Assert.AreEqual(true, regex.IsMatch("A2isax3456789121120912901212121"));            
-           Assert.AreEqual(true, regex.IsMatch("A2isax 219sa"));            
-           Assert.AreEqual(true, regex.IsMatch("A2isaxAAAA221"));            
-           Assert.AreEqual(true, regex.IsMatch("A2isaxAAAA221 "));            
+            Assert.AreEqual(false, regex.IsMatch(""));
+            Assert.AreEqual(false, regex.IsMatch(" "));
+            Assert.AreEqual(false, regex.IsMatch(" 123"));
+            Assert.AreEqual(true, regex.IsMatch("aaaaa"));
+            Assert.AreEqual(true, regex.IsMatch("1abic121"));
+            Assert.AreEqual(true, regex.IsMatch("A2isax"));
+            Assert.AreEqual(true, regex.IsMatch("A2isax3456789121120912901212121"));
+            Assert.AreEqual(true, regex.IsMatch("A2isax 219sa"));
+            Assert.AreEqual(true, regex.IsMatch("A2isaxAAAA221"));
+            Assert.AreEqual(true, regex.IsMatch("A2isaxAAAA221 "));
         }
 
         [TestMethod]
@@ -38,7 +38,7 @@ namespace UnitTestProject1
         {
             string pattern = @"^(?=(?:\d+)+7$)";
             Regex regex = new Regex(pattern);
-            Assert.AreEqual(true,regex.IsMatch("12134a567"));
+            Assert.AreEqual(true, regex.IsMatch("12134a567"));
         }
 
         [TestMethod]
@@ -47,8 +47,8 @@ namespace UnitTestProject1
             string pattern = @"\d{1,3}(.\d{1,3}){4}";
             Regex regex = new Regex(pattern);
             Assert.AreEqual(true, regex.IsMatch("192.168.2.12"));
-        }       
-        
+        }
+
         [TestMethod]
         public void TestMethod4()
         {
@@ -60,26 +60,26 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMothod5()
         {
-            RedisClient redisClient = new RedisClient("127.0.0.1",6379);
-            redisClient.Set("100","10001");
+            RedisClient redisClient = new RedisClient("127.0.0.1", 6379);
+            redisClient.Set("100", "10001");
 
             //var res = @"^(?!.*\s)(?!^[\u4e00-\u9fa5]+$)(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^.{8,16}$";
 
             //string regex = @"^$";
 
-           var s = redisClient.Get<string>("100");
+            var s = redisClient.Get<string>("100");
         }
 
         [TestMethod]
         public void TestMothod6()
         {
-           // var regex1 = @"^(?!)$";
+            // var regex1 = @"^(?!)$";
         }
 
         //0、1、1、2、3、5、8、13、21、34
         private void CalArr()
         {
-            
+
         }
 
         [TestMethod]
@@ -99,10 +99,24 @@ namespace UnitTestProject1
             }
             catch (System.Exception)
             {
-                
                 throw;
             }
 
+        }
+
+        [TestMethod]
+        public void SnowflakeTest()
+        {
+            var t = -1L ^ (-1L << 12);
+            var t1 = 4095L & t;
+            var t2 = 4096L & t;
+            var t3 = 4097L & t;
+            var t4 = 4098L & t;
+            var t5 = 4099L & t;
+            // var t1 = DateTime.Now.ToLongTimeString();
+            // var t2 = (DateTime.Now - (new DateTime(1970, 1, 1, 0, 0, 0))).TotalMilliseconds;
+            // var t3 = (long)(DateTime.Now - (new DateTime(1970, 1, 1, 0, 0, 0))).TotalMilliseconds;
+            // var t4 = -1L << 5;
         }
     }
 }
--- devitemapi/Infrastructure/Snowflake.cs
+++ devitemapi/Infrastructure/Snowflake.cs
@@ -1,7 +1,55 @@
+using System;
+
 namespace devitemapi.Infrastructure
 {
     public class Snowflake
     {
-        
+        private long _timestamp = -1L;
+
+        private long _lastTimestamp = -1L;
+
+        private long _sequence { get; set; }
+
+        private static int _sequenceBits = 12;
+        private long _sequenceMask = -1L ^ (-1L << _sequenceBits);
+
+        public long NextId()
+        {
+            var timestamp = GetTimestamp();
+            if (timestamp == _lastTimestamp)
+            {
+                _sequence = (_sequence + 1) & _sequenceMask;
+                if(_sequence == 0)
+                {
+                    timestamp = NextTimestamp(_lastTimestamp);
+                }
+            }
+            else
+            {
+                _sequence = 0;
+            }
+            _lastTimestamp = timestamp;
+            // 先将当前时间戳左移，放到41 bit那儿；将机房id左移放到5 bit那儿；将机器id左移放到5 bit那儿；将序号放最后12 bit
+            // 最后拼接起来成一个64 bit的二进制数字，转换成10进制就是个long型
+            // ((timestamp - INITIAL_TIME_STAMP) << TIMESTAMP_OFFSET)| (datacenterId << DATACENTERID_OFFSET)| (workerId << WORKERID_OFFSET)| sequence
+
+            //((timestamp-twepoch) << timestampLeftShift)|(datacenterId << datacenterIdShift)|(workerId << workerIdShift) | sequence;
+            return 0;
+        }
+
+        private long NextTimestamp(long lastTimestamp)
+        {
+            var timestamp = GetTimestamp();
+            while (timestamp <= lastTimestamp)
+            {
+                timestamp = GetTimestamp();
+            }
+            return timestamp;
+        }
+
+        private long GetTimestamp()
+        {
+            return (long)(DateTime.Now - (new DateTime(1970, 1, 1, 0, 0, 0))).TotalMilliseconds;
+        }
     }
 }
